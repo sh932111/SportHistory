a805336e65bc93fd753490c55a64006aeb5647a2
yojia <sh93211@gmail.com>
2014-11-03 09:11:05 +0800

    refresh point

--- js/index.js
+++ js/index.js
@@ -6,8 +6,8 @@ function setUI() {
 	setPageUtil('#pgHeader','PageUtil/Header/Header.html');
 	setPageUtil('#pgSilder','PageUtil/Silder/Silder.html');
 	setPageUtil('#pgLeft','PageUtil/pgLeft/pgLeft.html');
-	setPageUtil('#pgSelectBar','Page/SelectBar/SelectBar.html');
-	setPageUtil('#pgMain','PageUtil/Index/Index.html');
+	setPageUtil('#pgSelectBar','PageUtil/SelectBar/SelectBar.html');
+	setPageUtil('#pgMain','Page/Index/Index.html');
 	setPageUtil('#pgFooter','PageUtil/Footer/Footer.html');
 }
 
