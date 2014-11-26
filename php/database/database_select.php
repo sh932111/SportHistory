4a34aaac110dcc4f379c3a444606e67467db44a1
yojia <sh93211@gmail.com>
2014-11-26 21:13:22 +0800

    duapte

--- php/database/database_select.php
+++ php/database/database_select.php
@@ -1,7 +1,7 @@
 <?php
 
 function selectAllTable($link,$table_name) {
-	$action = sprintf("SELECT * FROM `$table_name` ORDER BY CAST(time AS signed) DESC");
+	$action = sprintf("SELECT * FROM `$table_name` ORDER BY CAST(date AS signed) DESC");
 	$obj = mysql_query($action, $link);
 	return $obj;
 }
