ef38dcfdf8433209a4e65f7be92afb0debdf8225
yojia <sh93211@gmail.com>
2014-11-26 21:18:23 +0800

    duapte

--- php/database/database_select.php
+++ php/database/database_select.php
@@ -1,7 +1,7 @@
 <?php
 
 function selectAllTable($link,$table_name) {
-	$action = sprintf("SELECT * FROM `$table_name` ORDER BY date");
+	$action = sprintf("SELECT * FROM `$table_name` ORDER BY date DESC");
 	$obj = mysql_query($action, $link);
 	return $obj;
 }
