4c27935c378d3fee0a396461373da77b9947f1aa
sh932111 <sh932111@gmail.com>
2014-11-27 23:57:34 +0800

    update

--- PageUtil/pgLeft/pgLeft.js
+++ PageUtil/pgLeft/pgLeft.js
@@ -11,28 +11,27 @@ function leftBarInit(){
 		var list = user_data.list;
 		for (var i = 0; i < list.length; i++) {
 			var path = list[i].img_path;
-			console.log(path);
-			slider_rsc.push("../SportServer/php/data/Msg/2014112618419/8.jpg");
+			slider_rsc.push(path);
 		}
-	}); 
 
-	var slider_rsc2 = ["img/test.jpg"];
+		var slider_rsc2 = ["img/test.jpg"];
 		
-	var sliderBtFrame = document.getElementById('NavBtFrame');
-	var sliderImg = document.getElementById('NavDetailImg');
-	var sliderBtFrame2 = document.getElementById('NavBtFrame2');
-	var sliderImg2 = document.getElementById('NavDetailImg2');
-	var bt_off = "navStyle";
-	var bt_on = "navSelectedStyle";
-	openSlider(slider_rsc ,sliderBtFrame,sliderImg,2500,sliders,slidersBt,bt_off,bt_on);
-	openSlider(slider_rsc2 ,sliderBtFrame2,sliderImg2,2500,sliders2,slidersBt2,bt_off,bt_on);
-	
-	var NavDetailImgBox1 = document.getElementById('NavDetailImgBox1');
-	NavDetailImgBox1.addEventListener("click", function(e){
+		var sliderBtFrame = document.getElementById('NavBtFrame');
+		var sliderImg = document.getElementById('NavDetailImg');
+		var sliderBtFrame2 = document.getElementById('NavBtFrame2');
+		var sliderImg2 = document.getElementById('NavDetailImg2');
+		var bt_off = "navStyle";
+		var bt_on = "navSelectedStyle";
+		openSlider(slider_rsc ,sliderBtFrame,sliderImg,2500,sliders,slidersBt,bt_off,bt_on);
+		openSlider(slider_rsc2 ,sliderBtFrame2,sliderImg2,2500,sliders2,slidersBt2,bt_off,bt_on);
 
-	});
-	var NavDetailImgBox2 = document.getElementById('NavDetailImgBox2');
-	NavDetailImgBox2.addEventListener("click", function(e){
+		var NavDetailImgBox1 = document.getElementById('NavDetailImgBox1');
+		NavDetailImgBox1.addEventListener("click", function(e){
 
-	});
+		});
+		var NavDetailImgBox2 = document.getElementById('NavDetailImgBox2');
+		NavDetailImgBox2.addEventListener("click", function(e){
+
+		});
+	}); 
 }
